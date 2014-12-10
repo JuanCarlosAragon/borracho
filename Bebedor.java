f1124097c6d4a8bad5371515baa51731b62fbe57
Juan Carlos Aragón <juankar.md@gmail.com>
2014-12-10 13:09:27 +0100

    Los bebedores ahora tienen dinero el cual se gastan en copas, ahora también pueden invitarse entre ellos

--- Bebedor.java
+++ Bebedor.java
@@ -12,33 +12,48 @@ public class Bebedor
    private int limite;
    private int cantidadDeAlcohol;
    private String nombre;
+   private int dinero;
    
-   public Bebedor(String nombre){
+   public Bebedor(String nombre, int dinero){
        numeroDeCopas = 0;
        borracho = false;
        limite = 100;
        cantidadDeAlcohol = 0;
        this.nombre = nombre;
+       this.dinero = dinero;
     }
     /*
      * Metodo que hace al borracho beberse un acopa elegida
      */
-   public void bebe (Cubata name){
-       Cubata copa = name;
-       //Aumentamos la cantidad de copas que se ha tomado
-       numeroDeCopas ++;
-       //aumentamos la cantidad de alcohol en sangre
-       cantidadDeAlcohol = cantidadDeAlcohol + copa.bebe();
-       //Comprobamos si el borracho ya se ha emborrachado
-       actualiza();
+   public void bebe (Cubata copa, boolean invitado){
        if(borracho == true){
            System.out.println("No gracias");
-           cantidadDeAlcohol = cantidadDeAlcohol - copa.bebe();
-           numeroDeCopas --;
         }
-       
-       
-       
+        else if(borracho == false && invitado == false){
+            if(dinero < copa.getPrecio()){
+                System.out.println("No me queda dinero, pide que me inviten!");
+            }
+            else{
+                //Aumentamos la cantidad de copas que se ha tomado
+                numeroDeCopas ++;
+                //aumentamos la cantidad de alcohol en sangre
+                cantidadDeAlcohol = cantidadDeAlcohol + copa.bebe();
+                //Hacemos que el borracho se gaste dinero si no le han invitado
+
+                dinero = dinero - copa.getPrecio();
+
+                //Comprobamos si el borracho ya se ha emborrachado
+                actualizaEstadoDeEmbriaguez();
+            }
+        }
+        else{
+                //Aumentamos la cantidad de copas que se ha tomado
+                numeroDeCopas ++;
+                //aumentamos la cantidad de alcohol en sangre
+                cantidadDeAlcohol = cantidadDeAlcohol + copa.bebe();
+                //Comprobamos si el borracho ya se ha emborrachado
+                actualizaEstadoDeEmbriaguez();
+        }
     }
    public void preguntame(String pregunta){
        if((pregunta.contains(nombre)) || (borracho == true)){
@@ -56,10 +71,23 @@ public class Bebedor
     /**
      * Metodo privado que actualiza si el borracho ya ha bebido suficiente
      */
-   private void actualiza(){
+   private void actualizaEstadoDeEmbriaguez(){
        if(cantidadDeAlcohol>limite){
            borracho = true;
         }
     }
-    
+   /**
+    * Hacemos que los borrachos puedan invitarse entre sí
+    */
+   public void invita(Bebedor invitado, Cubata copa){
+       if(dinero >= copa.getPrecio()){
+           invitado.bebe(copa, true);
+           dinero = dinero - copa.getPrecio();
+        }
+       else{
+           System.out.println("No puedo invitar, no me queda dinero," + 
+                                " que me inviten a mi!!");
+        }
+       
+    }
 }
--- Cubata.java
+++ Cubata.java
@@ -9,14 +9,19 @@ public class Cubata
 {
     private String nombre;
     private int cantidad;
-   public Cubata(String nombre, int cantidad){
+    private int precio;
+   public Cubata(String nombre, int cantidad, int precio){
        this.nombre = nombre;
        this.cantidad = cantidad;
+       this.precio = precio;
     }
     /**
      * Metodo que devuelve la cantidad de alcohol que hay en la copa
      */
    public int bebe(){
        return cantidad;
+    }
+   public int getPrecio(){
+       return precio;
     }
 }
